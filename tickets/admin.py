b5c67414921b1a0b8a0cd4260eada740babe2052
theomaro <theo.pombe@gmail.com>
2024-10-30 18:57:56 +0300

    register comment model to admin

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Centre, Ticket
+from .models import Centre, Ticket, Comment
 
 # Register your models here.
 
 admin.site.register(Centre)
 admin.site.register(Ticket)
+admin.site.register(Comment)
