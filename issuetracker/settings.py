159ccea2b601440afb2c9dc079072a4ade9ceb0f
theomaro <theo.pombe@gmail.com>
2024-10-30 12:11:50 +0300

    Change database engine from mysql to sqlite

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -77,11 +77,8 @@ WSGI_APPLICATION = "issuetracker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "issues",
-        "HOST": "localhost",
-        "USER": "root",
-        "PASSWORD": "0011",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
