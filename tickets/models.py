152fce5ab1e8c208592c33a2e99795cf38d15f75
theomaro <theo.pombe@gmail.com>
2024-10-30 12:11:58 +0300

    register Centre model

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Centre(models.Model):
+    number = models.CharField(max_length=254, unique=True, primary_key=True)
+    name = models.CharField(max_length=254)
+    region = models.CharField(max_length=254)
+    district = models.CharField(max_length=254)
+    email = models.EmailField(max_length=254, unique=True)
+    phone = models.CharField(max_length=245, unique=True)
+
+    class Meta:
+        unique_together = ["number", "name"]
+        ordering = ["name", "number"]
+
+    def __str__(self):
+        return f"{self.number} - {self.name}"
